c3d2d28af7d635c1ac71ab2e4600e6e399ec3325
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-03-04 23:38:39 +0800

    feat: created app-level templates and views for bookclub app, integrated urls

--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from .models import *
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    genres = Genre.objects.all()
+    ctx = {
+        'books': books,
+        'genres': genres
+    }
+
+    return render(request, 'bookclub/book_list.html', ctx)
+
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    ctx = {
+        'book': book,
+    }
+    return render(request, 'bookclub/book_detail.html', ctx)
