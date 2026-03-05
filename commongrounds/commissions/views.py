346899284ab207691eceae2c060785e5de00d2c2
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-06 02:01:05 +0800

    feature/commissions: connected pages to db
    - fixed get_absolute_url of Commission Model
    - updates each template html to display data and link properly back and forth

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+from commissions.models import Commission
 
 def requests_list(request):
-    return render(request, "requests_list.html")
+    return render(request, "requests_list.html", {"requests": Commission.objects.all()})
 
 def request_details(request, request_id):
-    return render(request, "request_details.html", {"request": {"id": request_id} })
+    return render(request, "request_details.html", {"request": Commission.objects.get(pk=request_id)})
