0c753f052bff9fbff925fcf74eebd75a469d20b1
Kenaz <66073275+Kencel@users.noreply.github.com>
2026-03-05 16:18:44 +0800

    feat: create views for product list and product details

--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Product, ProductType
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'item'
+    template_name = 'product_detail.html'
