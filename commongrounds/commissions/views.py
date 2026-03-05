fe6b0c04967e69a5a339621790c1678e4f656ea9
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-06 01:07:00 +0800

    feat: added basic routes

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def requests_list(request):
+    return render(request, "requests_list.html")
+
+def request_details(request, request_id):
+    return render(request, "request_details.html", {"request": {"id": request_id} })
