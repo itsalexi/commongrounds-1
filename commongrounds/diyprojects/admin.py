a70890af403913e1d68ff8ff6ce4fd425c04e6b4
jjscripts <pardojeromeimportant@gmail.com>
2026-03-05 18:39:30 +0800

    feat: added admin, created models, views, and templates

--- commongrounds/diyprojects/admin.py
+++ commongrounds/diyprojects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ProjectCategory, Project
 
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
 # Register your models here.
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import *
 
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        ProjectCategory, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def get_absolute_url(self):
+        return reverse('diyprojects:project_detail', args=[self.pk])
 # Create your models here.
