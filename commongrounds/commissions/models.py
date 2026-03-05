28773868147a83c47127d3e07d7a0d01c814efd7
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-06 01:18:37 +0800

    feat/commissions: added models

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+     
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    num_of_people_required = models.PositiveIntegerField() # Whole numbers are positive integers and 0
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['created_on']
