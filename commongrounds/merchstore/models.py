187f03429b4f107019357348ef784d4663603093
Kenaz <66073275+Kencel@users.noreply.github.com>
2026-03-05 16:11:29 +0800

    feat: create ProductType and Product models

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='product'
+    )
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=65)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
