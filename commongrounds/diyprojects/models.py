3c1ac1463dae71f630da87626d3f4d8dcac90e60
jjscripts <pardojeromeimportant@gmail.com>
2026-03-05 18:47:52 +0800

    feat: updated views and url

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import *
+from django.urls import reverse
 
 
 class ProjectCategory(models.Model):
--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -5,5 +5,5 @@ app_name = 'diyprojects'
 
 urlpatterns = [
     path('projects/', project_list, name='project_list'),
-    path('project/<int:pk>/', project_detail, name='project_detail'),
+    path('projects/<int:pk>/', project_detail, name='project_detail'),
 ]
