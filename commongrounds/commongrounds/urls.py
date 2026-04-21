f1c4a18ffc60062a9b9e5276b6ebbb519c322298
itsalexi <alexicanamo@gmail.com>
2026-04-21 22:01:34 +0800

    feat: create a landing page

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from .views import landing
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+    path('', landing, name='landing'),
     path('admin/', admin.site.urls),
     path('bookclub/', include('bookclub.urls', namespace='bookclub')),
     path('merchstore/', include('merchstore.urls', namespace='merchstore')),
