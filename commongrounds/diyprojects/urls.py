43fb19c8091ddd3160ea8e6e7a3e1c537aa3d487
jjscripts <pardojeromeimportant@gmail.com>
2026-05-05 17:32:48 +0800

    bug: change url

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import project_list, project_detail
+from .views import project_list, project_detail, project_create
 
 
 urlpatterns = [
     path('projects/', project_list, name='project_list'),
-    path('projects/<int:pk>/', project_detail, name='project_detail'),
+    path('project/create/', project_create, name='project_create'),
+    path('project/<int:pk>/', project_detail, name='project_detail'),
 ]
 
 app_name = 'diyprojects'
