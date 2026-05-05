be6137541677b58cd9e1cad49dfdd9a2aa8ef9c3
jjscripts <pardojeromeimportant@gmail.com>
2026-05-05 17:33:46 +0800

    feat: added models

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+from accounts.models import Profile
+
 
 class ProjectCategory(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
@@ -15,13 +17,17 @@ class ProjectCategory(models.Model):
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ProjectCategory, on_delete=models.SET_NULL, null=True
+    )
+    creator = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True
+    )
     description = models.TextField()
     materials = models.TextField()
     steps = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(
-        ProjectCategory, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -31,4 +37,32 @@ class Project(models.Model):
 
     def get_absolute_url(self):
         return reverse('diyprojects:project_detail', args=[self.pk])
-# Create your models here.
+
+
+class Favorite(models.Model):
+    class Status(models.TextChoices):
+        BACKLOG = 'Backlog'
+        TODO = 'To-Do'
+        DONE = 'Done'
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date_favorited = models.DateField(auto_now_add=True)
+    project_status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.BACKLOG,
+    )
+
+
+class ProjectReview(models.Model):
+    reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.TextField()
+    image = models.ImageField()
+
+
+class ProjectRating(models.Model):
+    SCORE_CHOICES = [(i, str(i)) for i in range(1, 11)]
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    score = models.IntegerField(choices=SCORE_CHOICES, default=1)
