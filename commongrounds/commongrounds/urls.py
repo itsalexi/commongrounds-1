1eaf8a8e5c5faedf2888ab80610eb743580754fa
itsalexi <alexicanamo@gmail.com>
2026-05-05 19:27:05 +0800

    feat: add media folder settings

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.static import serve
 from .views import landing
 
 urlpatterns = [
@@ -29,3 +32,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('events/<path:path>', serve, {'document_root': settings.MEDIA_ROOT / 'events'}),
+    ]
