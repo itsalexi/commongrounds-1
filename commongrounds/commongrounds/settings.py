f1a5f016a6dbeef5f7f6520deb286cce8a7e1bce
itsalexi <alexicanamo@gmail.com>
2026-05-06 13:55:38 +0800

    feat: add back localhost to allowedhosts

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['159.223.40.159', 'commongrounds.website', 'www.commonground.website']
+ALLOWED_HOSTS = ['159.223.40.159', 'commongrounds.website', 'www.commonground.website', 'localhost']
 
 
 # Application definition
