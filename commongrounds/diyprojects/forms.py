f925c70005297e1a6a06ecce9bc0edde987cd3a2
jjscripts <pardojeromeimportant@gmail.com>
2026-05-07 00:39:40 +0800

    feat: added advanced requirements (repositories)

--- commongrounds/diyprojects/forms.py
+++ commongrounds/diyprojects/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 
-from .models import Project, ProjectReview
+from .models import Project, ProjectRating, ProjectReview
 
 
 class ProjectReviewForm(forms.ModelForm):
     class Meta:
         model = ProjectReview
         fields = ['comment', 'image']
+
+
+class ProjectRatingForm(forms.ModelForm):
+    class Meta:
+        model = ProjectRating
+        fields = ['score']
 
 
 class ProjectForm(forms.ModelForm):
