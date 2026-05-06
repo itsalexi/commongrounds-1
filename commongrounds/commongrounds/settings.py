0697560b4b1f873cc4a5ea98eb5d7de2de238ed8
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-05-06 12:57:08 +0800

    chore: added DigitalOcean Droplet IP and domain to allowed hosts

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.223.40.159', 'commongrounds.website', 'www.commonground.website']
 
 
 # Application definition
