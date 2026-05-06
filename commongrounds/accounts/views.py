3ece72a14e8ccfc48417ef90190dfb373d5957a1
itsalexi <alexicanamo@gmail.com>
2026-05-06 10:15:52 +0800

    feat: style accounts pages + password reset flow

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -8,6 +8,10 @@ from .models import Profile
 
 def register(request):
     form = UserCreationForm(request.POST or None)
+    form.fields['username'].help_text = ''
+    form.fields['password1'].help_text = ''
+    form.fields['password2'].help_text = ''
+    form.fields['password2'].label = 'Confirm password'
     if form.is_valid():
         user = form.save()
         Profile.objects.create(user=user, display_name=user.username, email='')
