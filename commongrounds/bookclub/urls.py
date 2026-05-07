5fc627f255345b7dbf1eba4ede29b4bac87ef274
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-05-07 09:50:56 +0800

    feat: updated existing views & urls, implemented views & urls for  BookReview, Adding Books, Editing Books, Borrowing Books, and Bookmarking

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -3,7 +3,11 @@ from .views import *  # from .views in blogpage, import index function
 
 urlpatterns = [
     path('books', book_list, name='book_list'),
+    path('book/add', book_add, name='book_add'),
     path('book/<int:pk>', book_detail, name='book_detail'),
+    path('book/<int:pk>/borrow', book_borrow, name='book_borrow'),
+    path('book/<int:pk>/edit', book_update, name='book_update'),
+    path('book/<int:pk>/bookmark', bookmark_toggle, name='bookmark_toggle'),
 ]
 
 app_name = 'bookclub'
