5d28b198e6f3a3f3336929993b82e42a9306219a
itsalexi <alexicanamo@gmail.com>
2026-05-07 11:43:38 +0800

    feat: add styling to the merch store templates

--- commongrounds/merchstore/urls.py
+++ commongrounds/merchstore/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from accounts.decorators import role_required
+
+from .views import ProductCreateView, ProductDetailView, ProductListView
 
 urlpatterns = [
     path('items/', ProductListView.as_view(), name='product_list'),
+    path(
+        'item/add',
+        role_required('Market Seller')(ProductCreateView.as_view()),
+        name='product_create',
+    ),
     path('item/<int:pk>', ProductDetailView.as_view(), name='product_detail')
 ]
 
