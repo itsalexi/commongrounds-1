1046ef93259697959c6a2c7b84283cd36168db03
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-05-07 09:37:47 +0800

    feat: updated Book and Genre models & admin, created Book Review, Bookmark, and Borrow models & admin

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Genre
+from .models import Book, Bookmark, BookReview, Borrow, Genre
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,5 +10,20 @@ class GenreAdmin(admin.ModelAdmin):
     model = Genre
 
 
+class BookReviewAdmin(admin.ModelAdmin):
+    model = BookReview
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    model = Bookmark
+
+
+class BorrowAdmin(admin.ModelAdmin):
+    model = Borrow
+
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Genre, GenreAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Borrow, BorrowAdmin)
